444bd0cb2966b4ce5c1a1bedefe777fd61bee2a3
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-14 15:15:32 -0500

    Initial Commit

---
+++ ipad Demo/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  ipad Demo
+//
+//  Created by Mohammad Kiani on 2020-01-14.
+//  Copyright © 2020 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet weak var detailDescriptionLabel: UILabel!
+
+
+    func configureView() {
+        // Update the user interface for the detail item.
+        if let detail = detailItem {
+            if let label = detailDescriptionLabel {
+                label.text = detail.description
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        configureView()
+    }
+
+    var detailItem: NSDate? {
+        didSet {
+            // Update the view.
+            configureView()
+        }
+    }
+
+
+}
+
